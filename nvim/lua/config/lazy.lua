568f85b51da6454815ad7467f30db37c767946cd
JoshQuaintance <jhquaintance@winsupply.com>
2026-09-18 11:31:19 -0400

    fix(update): prevent interactive pager in dotupdate and move nvim lazy-lock to local state

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
+  lockfile = vim.fn.stdpath("state") .. "/lazy/lazy-lock.json",
   defaults = {
     lazy = false,
     version = false,
